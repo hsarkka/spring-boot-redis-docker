c320b680202be281ec3f39a4afaef5c796bf3310
Heikki Särkkä <heikki@iki.fi>
2019-03-05 13:36:58 +0200

    Don't require rebuilding backend image after code changes

--- build-backend.sh
+++ build-backend.sh
@@ -1,14 +1,22 @@
 #!/bin/sh
 
+APP_NAME=demo
+
+GRADLE_CACHE_VOLUME=${APP_NAME}-gradle-cache
+
 echo "Creating volume for Gradle cache..."
-docker volume create --name gradle-cache
-echo
+docker volume create --name ${GRADLE_CACHE_VOLUME}
 
+echo
 echo "Running Gradle build in container..."
-docker run --rm --volume gradle-cache:/home/gradle/.gradle --volume "$PWD/backend":/home/gradle/project -w /home/gradle/project gradle:5.2-jdk8-alpine gradle clean build
-echo
+docker run \
+    --rm \
+    --volume ${GRADLE_CACHE_VOLUME}:/home/gradle/.gradle \
+    --volume ${PWD}/backend:/home/gradle/project \
+    --workdir /home/gradle/project \
+    gradle:5.2-jdk8-alpine gradle clean build
 
+echo
 echo "Build output:"
-BUILD_OUTPUT=backend/build/libs/demo-0.0.1-SNAPSHOT.jar
+BUILD_OUTPUT=backend/build/libs/${APP_NAME}-0.0.1-SNAPSHOT.jar
 ls -l $BUILD_OUTPUT
-cp $BUILD_OUTPUT docker/backend/app.jar
